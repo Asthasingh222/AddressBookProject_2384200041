61422aa5c9f2981b21fcea58b066c988308d7a83
Astha Singh <77485251+Asthasingh222@users.noreply.github.com>
2025-03-18 11:03:05 +0530

    [Astha]Add: section2 uploaded in AddressBook project

--- AddressBookProject/Program.cs
+++ AddressBookProject/Program.cs
@@ -1,5 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using RepositoryLayer.Context;
+using ModelLayer.Validator;
+using FluentValidation;
+using BusinessLayer.AutoMapper;
+using AutoMapper;
+using RepositoryLayer.Interface;
+using RepositoryLayer.Service;
+using BusinessLayer.Interface;
+using BusinessLayer.Service;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +17,23 @@ builder.Services.AddControllers();
 
 var connectionString = builder.Configuration.GetConnectionString("SqlConnection");
 builder.Services.AddDbContext<AddressBookContext>(options => options.UseSqlServer(connectionString));
+
+// Register AutoMapper
+
+var mapperConfig = new MapperConfiguration(cfg =>
+{
+    cfg.AddProfile<Mapping>();
+});
+
+IMapper mapper = mapperConfig.CreateMapper();
+builder.Services.AddSingleton(mapper);
+
+builder.Services.AddScoped<IAddressBookRL, AddressBookRL>();
+builder.Services.AddScoped<IAddressBookBL, AddressBookBL>();
+
+// Register FluentValidation
+
+builder.Services.AddValidatorsFromAssemblyContaining<AddressBookValidator>();
 
 var app = builder.Build();
 
